0078d2992d774ddcbe60640ba9f16878c8b97179
JoshuaEscalada <jescalada@addu.edu.ph>
2024-12-11 19:17:04 +0800

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,1 +1,9 @@
+function showSelectedCode() {
+    const selectedCode = document.getElementById('codeSelection').value;
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => section.classList.remove('active'));
+    if (selectedCode !== 'none') {
+        document.getElementById(selectedCode).classList.add('active');
+    }
+}
 
