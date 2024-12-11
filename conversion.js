4114f73d05ae0bd5dcf484c8474c6ec9d508e786
JoshuaEscalada <jescalada@addu.edu.ph>
2024-12-11 19:47:58 +0800

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -1,12 +1,3 @@
-function showSelectedCode() {
-    const selectedCode = document.getElementById('codeSelection').value;
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => section.classList.remove('active'));
-    if (selectedCode !== 'none') {
-        document.getElementById(selectedCode).classList.add('active');
-    }
-}
-
 function convertCelsiusToFahrenheit() {
     const celsius = parseFloat(document.getElementById('celsius').value);
     if (!isNaN(celsius)) {
