dd0d48979f03d6175a38d8470e3e577a33d47625
JoshuaEscalada <jescalada@addu.edu.ph>
2024-12-11 19:18:36 +0800

    Update taxcalculator.js

--- taxcalculator.js
+++ taxcalculator.js
@@ -1,1 +1,8 @@
-
+function showSelectedCode() {
+    const selectedCode = document.getElementById('codeSelection').value;
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => section.classList.remove('active'));
+    if (selectedCode !== 'none') {
+        document.getElementById(selectedCode).classList.add('active');
+    }
+}
