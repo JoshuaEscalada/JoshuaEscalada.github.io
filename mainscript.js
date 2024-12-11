d7437e34a64fac129ff14c6f7bc9ed8e81c11912
JoshuaEscalada <jescalada@addu.edu.ph>
2024-12-11 21:43:31 +0800

    Create mainscript.js

---
+++ mainscript.js
@@ -0,0 +1,8 @@
+function backToMenu() {
+    window.location.href = "index.html";
+}
+
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => section.style.display = 'none');
+    document.getElementById(sectionId).style.display = 'block';
+}
